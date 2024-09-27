c8e2d5509da4de4b9bca70c4035685a31dd4f330
Invaleleo <leonardo.silva1805@etec.sp.gov.br>
2024-09-27 10:18:40 -0300

    Aqui ó

--- src/screens/Add_Tarefa.js
+++ src/screens/Add_Tarefa.js
@@ -8,9 +8,14 @@ import {
     TouchableWithoutFeedback,
     Modal 
 } from "react-native"
+
+import Icon from "react-native-vector-icons/FontAwesome6";
+import moment from "moment";
+import DateTimePicker from "@react-native-community/datetimepicker"//digitar a data
 // Modal - recursos de tela sobrepostas, chama um tela em cima da outra.
 export default class Add_Tarefa extends Component{
     render(){
+        const data_formatada = moment(new Date()).format('ddd, D [de] MMM [de] YYYY') {/* variavel para data */}
         return(
             <Modal
                 transparent={true}
@@ -22,9 +27,21 @@ export default class Add_Tarefa extends Component{
                 </TouchableWithoutFeedback>
                 <View style={styles.principal}>
                     <Text style={Styles.cabecalho} > Nova Tarefa </Text>
-                    <TextInput
-                        placeholder="Descrição da Tarefa" 
-                    />{/* campo de texto */}
+                    <View style={Styles.container}>
+                        <Icon name="clipboard-check" size={25} ></Icon>
+                        <TextInput 
+                        style={Styles.input}
+                            placeholder="Descrição da Tarefa" 
+                        />{/* campo de texto */}
+                    </View>
+                    
+                    {/*parte para calendario */}
+                    <TouchableOpacity style={styles.container}>
+                        <Icon name="calendar" size={25}>
+                            <Text style={styles.data}>{data_formatada}</Text>
+                        </Icon>
+                    </TouchableOpacity>
+
                     <View>
                     <TouchableOpacity>
                         <Text>Cancelar</Text>
@@ -32,7 +49,7 @@ export default class Add_Tarefa extends Component{
                     <TouchableOpacity>
                         <Text>Salvar</Text>
                     </TouchableOpacity>
-                </View>
+                    </View>
                 </View>
 
                 
@@ -59,5 +76,30 @@ const Styles = StyleSheet.create({
         textAlign: 'center',
         padding: 20,
         fontSize: 25
+    },
+    input:{
+        width: '85%',
+        margin: 15,
+        borderWidth: 1,
+        borderColor: '#E3E3E3',
+        borderRadius: 6
+    },
+    botoes:{
+        flexDirection: 'row',
+        justifyContent: 'flex-end'       
+    },
+    botao:{
+        margin: 20,
+        marginRight: 30,
+        color: '#B13B44'
+    },
+    container:{
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginLeft: 15,
+        paddingVertical: 10
+    },
+    data:{
+        margin: 10,
     }
 })
